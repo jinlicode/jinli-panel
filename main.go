b207d02cb805737b7ce826a79d4c1c3bea1437c5
MSG-maniac <maniac.cn@gmail.com>
2020-09-06 14:47:23 +0000

    重新push

--- main.go
+++ main.go
@@ -13,13 +13,14 @@ func main() {
 	router.StaticFile("/", "./html/index.html")
 
 	v1 := router.Group("/v1")
-    {
-        v1.GET("/login", func(c *gin.Context){
+	{
+		v1.GET("/login", func(c *gin.Context) {
 			c.JSON(200, gin.H{
 				"message": "login",
 			})
 		})
-    }
+	}
 
 	router.Run("0.0.0.0:9527")
+
 }
