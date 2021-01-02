a9f6ab6ef5891d1da674c700bd01331f07a2c5df
MSG-maniac <maniac.cn@gmail.com>
2021-01-02 15:40:29 +0000

    修正路由错误

--- routers/router.go
+++ routers/router.go
@@ -29,7 +29,7 @@ func InitRouter() *gin.Engine {
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
-		router.StaticFS("/static", statikFS)
+		router.StaticFS("/", statikFS)
 	}
 
 	v1 := router.Group("/v1")
