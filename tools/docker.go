c8c33d87db8d287ee5881816b5cf4fa7250cc36b
hhf <5375530@qq.com>
2021-01-05 14:55:15 +0800

    新增检测容器运行状态函数

--- tools/docker.go
+++ tools/docker.go
@@ -68,3 +68,31 @@ func GetDockerIP(containersName string) string {
 	IPAddress := dataArr["NetworkSettings"].(map[string]interface{})["Networks"].(map[string]interface{})[containersName+"_net"].(map[string]interface{})["IPAddress"].(string)
 	return IPAddress
 }
+
+// CheckDockerStatus 获取容器的运行状态
+func CheckDockerStatus(containersName string) bool {
+	httpc := http.Client{
+		Transport: &http.Transport{
+			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
+				return net.Dial("unix", sockAddr)
+			},
+		},
+	}
+
+	resp, err := httpc.Get("http://localhost/v1.40/containers/" + containersName + "/json")
+	if err != nil {
+		return false
+	}
+
+	resp.Header.Set("Content-Type", "application/json")
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	dataArr := make(map[string]interface{})
+
+	json.Unmarshal(body, &dataArr)
+
+	Running := dataArr["State"].(map[string]interface{})["Running"].(bool)
+	return Running
+}
